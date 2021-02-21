1ea08b7ea0ed30cab63a06f43f141bcf94f7bdf8
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2021-02-21 17:05:49 +0100

    feat(prueba): Prueba helloworld en local OK

---
+++ app/Http/Controllers/HelloWorld.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorld extends Controller
+{
+    function prueba() {
+        return "Hello World!";
+    }
+}
