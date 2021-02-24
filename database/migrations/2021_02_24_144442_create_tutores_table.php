f6f8549b06f3bf91713ac67b7751727b939b773e
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2021-02-24 15:59:22 +0100

    arreglar migracion cambiar enum por strings

--- database/migrations/2021_02_24_144442_create_tutores_table.php
+++ database/migrations/2021_02_24_144442_create_tutores_table.php
@@ -16,14 +16,14 @@ class CreateTutoresTable extends Migration
         Schema::create('tutores', function (Blueprint $table) {
             $table->id();
             $table->string('nombre_empresa');
-            $table->enum('tipo_documento', ['NIF', 'NIE']);
+            $table->string('tipo_documento');
             $table->string('documento_identidad');
             $table->string('nombre_tutor');
             $table->string('primer_apellido_tutor');
             $table->string('segundo_apellido_tutor')->nullable();
-            $table->enum('pais_documento', ['ESPANYA', 'ECUADOR', 'IRLANDA']);
-            $table->enum('provincia', ['baleares', 'catalunya', 'madrid']);
-            $table->enum('municipio', ['palma', 'barcelona', 'Vallecas'])->nullable();
+            $table->string('pais_documento');
+            $table->string('provincia');
+            $table->string('municipio')->nullable();
             $table->enum('estado', ['tutor en activo', 'tutor inactivo']);
             $table->string('telefono')->nullable();
             $table->string('email');
